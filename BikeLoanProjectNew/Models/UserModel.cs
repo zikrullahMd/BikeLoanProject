c350d64799932334d8e327a1bc0c255798242d3e
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2022-04-08 22:34:11 +0530

    adding backend

---
+++ BikeLoanProjectNew/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeLoanProjectNew.Models
+{
+    public class UserModel
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string username { get; set; }
+        public string mobileNumber { get; set; }
+        public string userRole { get; set; }
+    }
+}
